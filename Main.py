5f2f66b0ddb063f1d97045e6bbd3b7644707c516
omar <oximas2004@gmail.com>
2025-09-28 16:38:22 +0300

    Add main.py to run the program from

---
+++ Main.py
@@ -0,0 +1,7 @@
+from GUI import GUI
+import Database as DB
+
+if __name__ == "__main__":
+    print("You are now using the Personal Financial Tracker made by Oximas")
+    app = GUI()
+    app.run()
